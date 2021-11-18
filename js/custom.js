1ccc791af0fa1d4155a3966810fc118f1f930f2e
Franco Bellucci <belluccifranco@gmail.com>
2021-11-18 21:25:36 +0100

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+(function($) {
+  let toggle = document.getElementById("menu-toggle");
+  let menu = document.getElementById("menu");
+  let close = document.getElementById("menu-close");
+
+  toggle.addEventListener("click", function(e) {
+    if (menu.classList.contains("open")) {
+      menu.classList.remove("open");
+    } else {
+      menu.classList.add("open");
+    }
+  });
+
+  close.addEventListener("click", function(e) {
+    menu.classList.remove("open");
+  });
+
+  // Close menu after click on smaller screens
+  $(window).on("resize", function() {
+    if ($(window).width() < 846) {
+      $(".main-menu a").on("click", function() {
+        menu.classList.remove("open");
+      });
+    }
+  });
+
+  $(".hover").mouseleave(function() {
+    $(this).removeClass("hover");
+  });
+
+})(jQuery);
